fc1b60a49827d4c0b42c42958b49bdd2f8d02e7f
Matt Stott <mjs@Matts-MBP-2.fritz.box>
2022-11-08 21:58:18 +0100

    update

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from comics import ForbiddenPlanet
 from schedule import every, repeat, run_pending
+import time
 
-@repeat(every(1).minutes)
+
+
+#@repeat(every(1).minutes)
+def main_stock_update():
+    ForbiddenPlanet(get_live_sitemap=True)
+
+
+@repeat(every().day.at("21:05"))
+def main_watchlist():
+    push = ForbiddenPlanet(get_live_sitemap=False)
+    push.run_watchlist_notification()
+
+
 def main(mode='watchlist'):
 
     if mode == 'stock_update':
@@ -19,4 +32,7 @@ def main(mode='watchlist'):
             push.run_livelist_notification()
     
 if __name__ == '__main__':
-    main()
+    while True:
+        run_pending()
+        time.sleep(1)
+    #main()
